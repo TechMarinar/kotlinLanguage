7c542df5c3826eb17ad2fd536b3d15b1f65810aa
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2022-10-14 17:26:50 +0200

    [smap] change offset for immutable variables at inline phase

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/inline/FunctionInlining.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/inline/FunctionInlining.kt
@@ -653,7 +653,7 @@ class FunctionInlining(
                     it.argumentExpression.transform( // Arguments may reference the previous ones - substitute them.
                         substitutor,
                         data = null
-                    )
+                    ).let { getValue -> IrGetValueWithoutLocation((getValue as IrGetValue).symbol) }
                 } else {
                     val newVariable =
                         currentScope.scope.createTemporaryVariable(
@@ -705,7 +705,7 @@ class FunctionInlining(
                 val variableInitializer = argument.argumentExpression.transform(substitutor, data = null)
 
                 if (argument.isImmutableVariableLoad) {
-                    substituteMap[argument.parameter] = variableInitializer
+                    substituteMap[argument.parameter] = IrGetValueWithoutLocation((variableInitializer as IrGetValue).symbol)
                     return@forEach
                 }
 
