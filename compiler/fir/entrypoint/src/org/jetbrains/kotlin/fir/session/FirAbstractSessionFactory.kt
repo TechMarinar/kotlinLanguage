JetBrains/kotlin

FIR JS: prepare session factory for JS/KLib usage

compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirAbstractSessionFactory.kt
@@ -71,7 +71,7 @@ abstract class FirAbstractSessionFactory {
         analyzerServices: PlatformDependentAnalyzerServices
     ): FirModuleData {
         return DependencyListForCliModule.createDependencyModuleData(
-            Name.special("<builtins of ${parentModuleName.identifier}"),
+            Name.special("<builtins of ${parentModuleName.asString()}"),
             platform,
             analyzerServices,
         )

TechMarinar/kotlinLanguage

compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirAbstractSessionFactory.kt
@@ -50,7 +50,7 @@ abstract class FirAbstractSessionFactory {
             register(FirKotlinScopeProvider::class, kotlinScopeProvider)
 
             val builtinsModuleData = DependencyListForCliModule.createDependencyModuleData(
-                Name.special("<builtins of ${mainModuleName.identifier}"),
+                Name.special("<builtins of ${mainModuleName.asString()}"),
                 moduleDataProvider.platform,
                 moduleDataProvider.analyzerServices,
             )