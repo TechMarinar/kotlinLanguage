JetBrains/kotlin

[Analysis API] rework renderer
^KTIJ-23268 fixed

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/declarations/impl/KtDeclarationRendererForSource.kt
@@ -1,0 +1,83 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.renderer.declarations.impl
+
+import org.jetbrains.kotlin.analysis.api.renderer.base.KtKeywordRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.base.annotations.KtAnnotationRendererForSource
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtCallableReturnTypeFilter
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtDeclarationRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtRecommendedRendererCodeStyle
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtRendererTypeApproximator
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.bodies.*
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.modifiers.impl.KtDeclarationModifiersRendererForSource
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.*
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.callables.*
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtAnonymousObjectSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtNamedClassOrObjectSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtSingleTypeParameterSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtTypeAliasSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypeListRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypesCallArgumentsRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypesFilter
+import org.jetbrains.kotlin.analysis.api.renderer.types.impl.KtTypeRendererForSource
+
+public object KtDeclarationRendererForSource {
+    public val WITH_QUALIFIED_NAMES: KtDeclarationRenderer = KtDeclarationRenderer {
+        nameRenderer = KtDeclarationNameRenderer.QUOTED
+        keywordRender = KtKeywordRenderer.AS_WORD
+        codeStyle = KtRecommendedRendererCodeStyle
+        modifiersRenderer = KtDeclarationModifiersRendererForSource.NO_IMPLICIT_MODIFIERS
+        classifierBodyRenderer = KtClassifierBodyRenderer.NO_BODY
+        bodyMemberScopeProvider = KtRendererBodyMemberScopeProvider.ALL_DECLARED
+        bodyMemberScopeSorter = KtRendererBodyMemberScopeSorter.ENUM_ENTRIES_AT_BEGINING
+        superTypesRenderer = KtSuperTypeListRenderer.AS_LIST
+        superTypesFilter = KtSuperTypesFilter.NO_DEFAULT_TYPES
+        superTypesArgumentRenderer = KtSuperTypesCallArgumentsRenderer.EMPTY_PARENS
+        declarationTypeRenderer = KtDeclarationTypeRenderer.NO_IMPLICIT_TYPES
+        functionLikeBodyRenderer = KtFunctionLikeBodyRenderer.NO_BODY
+        valueParametersRenderer = KtCallableParameterRenderer.PARAMETERS_IN_PARENS
+
+        typeParametersRenderer = KtTypeParametersRenderer.WITH_BOUNDS_IN_WHERE_CLAUSE
+        classInitializerRender = KtClassInitializerRenderer.INIT_BLOCK_WITH_BRACES
+
+        anonymousFunctionRenderer = KtAnonymousFunctionSymbolRenderer.AS_SOURCE
+        backingFieldRenderer = KtBackingFieldSymbolRenderer.AS_FIELD_KEYWROD
+        constructorRenderer = KtConstructorSymbolRenderer.AS_SOURCE
+        enumEntryRenderer = KtEnumEntrySymbolRenderer.AS_SOURCE
+        functionSymbolRenderer = KtFunctionSymbolRenderer.AS_SOURCE
+        javaFieldRenderer = KtJavaFieldSymbolRenderer.AS_SOURCE
+        localVariableRenderer = KtLocalVariableSymbolRenderer.AS_SOURCE
+        getterRenderer = KtPropertyGetterSymbolRenderer.AS_SOURCE
+        setterRenderer = KtPropertySetterSymbolRenderer.AS_SOURCE
+        propertyRenderer = KtKotlinPropertySymbolRenderer.AS_SOURCE
+        kotlinPropertyRenderer = KtKotlinPropertySymbolRenderer.AS_SOURCE
+        syntheticJavaPropertyRenderer = KtSyntheticJavaPropertySymbolRenderer.AS_SOURCE
+        valueParameterRenderer = KtValueParameterSymbolRenderer.AS_SOURCE
+        samConstructorRenderer = KtSamConstructorSymbolRenderer.NOT_RENDER
+
+        callableSignatureRenderer = KtCallableSignatureRender.FOR_SOURCE
+        accessorBodyRenderer = KtPropertyAccessorBodyRenderer.NO_BODY
+        parameterDefaultValueRenderer = KtParameterDefaultValueRenderer.NO_DEFAULT_VALUE
+        variableInitializerRenderer = KtVariableInitializerRenderer.NO_INITIALIZER
+
+        classOrObjectRenderer = KtNamedClassOrObjectSymbolRenderer.AS_SOURCE
+        typeAliasRenderer = KtTypeAliasSymbolRenderer.AS_SOURCE
+        anonymousObjectRenderer = KtAnonymousObjectSymbolRenderer.AS_SOURCE
+        singleTypeParameterRenderer = KtSingleTypeParameterSymbolRenderer.WITHOUT_BOUNDS
+        propertyAccessorsRenderer = KtPropertyAccessorsRenderer.NO_DEFAULT
+
+        typeRenderer = KtTypeRendererForSource.WITH_QUALIFIED_NAMES
+        annotationRenderer = KtAnnotationRendererForSource.WITH_QUALIFIED_NAMES
+        declarationTypeApproximator = KtRendererTypeApproximator.TO_DENNOTABLE
+        returnTypeFilter = KtCallableReturnTypeFilter.NO_UNIT
+    }
+
+    public val WITH_SHORT_NAMES: KtDeclarationRenderer = WITH_QUALIFIED_NAMES.with {
+        annotationRenderer = KtAnnotationRendererForSource.WITH_SHORT_NAMES
+        typeRenderer = KtTypeRendererForSource.WITH_SHORT_NAMES
+    }
+}
+

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/declarations/renderers/classifiers/KtAnonymousObjectSymbolRenderer.kt
@@ -1,0 +1,33 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers
+
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtDeclarationRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderAnnotationsAndModifiers
+import org.jetbrains.kotlin.analysis.api.symbols.KtAnonymousObjectSymbol
+import org.jetbrains.kotlin.analysis.utils.printer.PrettyPrinter
+import org.jetbrains.kotlin.lexer.KtTokens
+
+public interface KtAnonymousObjectSymbolRenderer {
+    context(KtAnalysisSession, KtDeclarationRenderer)
+    public fun renderSymbol(symbol: KtAnonymousObjectSymbol, printer: PrettyPrinter)
+
+    public object AS_SOURCE : KtAnonymousObjectSymbolRenderer {
+        context(KtAnalysisSession, KtDeclarationRenderer)
+        override fun renderSymbol(symbol: KtAnonymousObjectSymbol, printer: PrettyPrinter): Unit = printer {
+            " ".separated(
+                {
+                    " : ".separated(
+                        { renderAnnotationsAndModifiers(symbol, printer, KtTokens.OBJECT_KEYWORD) },
+                        { superTypesRenderer.renderSuperTypes(symbol, printer) }
+                    )
+                },
+                { classifierBodyRenderer.renderBody(symbol, printer) },
+            )
+        }
+    }
+}

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/types/impl/KtTypeRendererForSource.kt
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.renderer.types.impl
+
+import org.jetbrains.kotlin.analysis.api.renderer.base.KtKeywordRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.base.annotations.KtAnnotationRendererForSource
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtRendererTypeApproximator
+import org.jetbrains.kotlin.analysis.api.renderer.types.KtTypeRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.types.renders.*
+
+public object KtTypeRendererForSource {
+    public val WITH_QUALIFIED_NAMES: KtTypeRenderer = KtTypeRenderer {
+        capturedTypeRenderer = KtCapturedTypeRenderer.AS_RPOJECTION
+        definitelyNotNullTypeRenderer = KtDefinitelyNotNullTypeRenderer.AS_TYPE_INTERSECTION
+        dynamicTypeRenderer = KtDynamicTypeRenderer.AS_DYNAMIC_WORD
+        flexibleTypeRenderer = KtFlexibleTypeRenderer.AS_SHORT
+        functionalTypeRenderer = KtFunctionalTypeRenderer.AS_FUNCTIONAL_TYPE
+        integerLiteralTypeRenderer = KtIntegerLiteralTypeRenderer.AS_ILT_WITH_VALUE
+        intersectionTypeRenderer = KtIntersectionTypeRenderer.AS_INTERSECTION
+        typeErrorTypeRenderer = KtTypeErrorTypeRenderer.AS_CODE_IF_POSSIBLE
+        typeParameterTypeRenderer = KtTypeParameterTypeRenderer.AS_SOURCE
+        unresolvedClassErrorTypeRenderer = KtUnresolvedClassErrorTypeRenderer.UNRESOLVED_QUALIFIER
+        usualClassTypeRenderer = KtUsualClassTypeRenderer.AS_CLASS_TYPE_WITH_TYPE_ARGUMENTS
+        classIdRenderer = KtClassTypeQualifierRenderer.WITH_QUALIFIED_NAMES
+        typeNameRenderer = KtTypeNameRendererForSource.QUOTED
+        typeApproximator = KtRendererTypeApproximator.TO_DENNOTABLE
+        typeProjectionRenderer = KtTypeProjectionRenderer.WITH_VARIANCE
+        annotationsRender = KtAnnotationRendererForSource.WITH_QUALIFIED_NAMES
+        keywordRenderer = KtKeywordRenderer.AS_WORD
+    }
+
+    public val WITH_SHORT_NAMES: KtTypeRenderer = WITH_QUALIFIED_NAMES.with {
+        classIdRenderer = KtClassTypeQualifierRenderer.WITH_SHORT_NAMES_WITH_NESTED_CLASSIFIERS
+        annotationsRender = KtAnnotationRendererForSource.WITH_SHORT_NAMES
+    }
+}

TechMarinar/kotlinLanguage

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/declarations/impl/KtDeclarationRendererForSource.kt
@@ -1,0 +1,88 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.renderer.declarations.impl
+
+import org.jetbrains.kotlin.analysis.api.renderer.base.KtKeywordRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.base.annotations.KtAnnotationRendererForSource
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtCallableReturnTypeFilter
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtDeclarationRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtRecommendedRendererCodeStyle
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtRendererTypeApproximator
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.bodies.*
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.modifiers.impl.KtDeclarationModifiersRendererForSource
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.*
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.callables.*
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtAnonymousObjectSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtNamedClassOrObjectSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtSingleTypeParameterSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers.KtTypeAliasSymbolRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypeListRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypeRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypesCallArgumentsRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.superTypes.KtSuperTypesFilter
+import org.jetbrains.kotlin.analysis.api.renderer.types.impl.KtTypeRendererForSource
+
+public object KtDeclarationRendererForSource {
+    public val WITH_QUALIFIED_NAMES: KtDeclarationRenderer = KtDeclarationRenderer {
+        nameRenderer = KtDeclarationNameRenderer.QUOTED
+        keywordRender = KtKeywordRenderer.AS_WORD
+        codeStyle = KtRecommendedRendererCodeStyle
+        modifiersRenderer = KtDeclarationModifiersRendererForSource.NO_IMPLICIT_MODIFIERS
+        classifierBodyRenderer = KtClassifierBodyRenderer.NO_BODY
+        bodyMemberScopeProvider = KtRendererBodyMemberScopeProvider.ALL_DECLARED
+        bodyMemberScopeSorter = KtRendererBodyMemberScopeSorter.ENUM_ENTRIES_AT_BEGINING
+
+        superTypeRenderer = KtSuperTypeRenderer.WITH_OUT_APPROXIMATION
+        superTypeListRenderer = KtSuperTypeListRenderer.AS_LIST
+        superTypesFilter = KtSuperTypesFilter.NO_DEFAULT_TYPES
+        superTypesArgumentRenderer = KtSuperTypesCallArgumentsRenderer.EMPTY_PARENS
+        functionLikeBodyRenderer = KtFunctionLikeBodyRenderer.NO_BODY
+        valueParametersRenderer = KtCallableParameterRenderer.PARAMETERS_IN_PARENS
+
+        typeParametersRenderer = KtTypeParametersRenderer.WITH_BOUNDS_IN_WHERE_CLAUSE
+        classInitializerRender = KtClassInitializerRenderer.INIT_BLOCK_WITH_BRACES
+
+        anonymousFunctionRenderer = KtAnonymousFunctionSymbolRenderer.AS_SOURCE
+        backingFieldRenderer = KtBackingFieldSymbolRenderer.AS_FIELD_KEYWROD
+        constructorRenderer = KtConstructorSymbolRenderer.AS_SOURCE
+        enumEntryRenderer = KtEnumEntrySymbolRenderer.AS_SOURCE
+        functionSymbolRenderer = KtFunctionSymbolRenderer.AS_SOURCE
+        javaFieldRenderer = KtJavaFieldSymbolRenderer.AS_SOURCE
+        localVariableRenderer = KtLocalVariableSymbolRenderer.AS_SOURCE
+        getterRenderer = KtPropertyGetterSymbolRenderer.AS_SOURCE
+        setterRenderer = KtPropertySetterSymbolRenderer.AS_SOURCE
+        propertyRenderer = KtKotlinPropertySymbolRenderer.AS_SOURCE
+        kotlinPropertyRenderer = KtKotlinPropertySymbolRenderer.AS_SOURCE
+        syntheticJavaPropertyRenderer = KtSyntheticJavaPropertySymbolRenderer.AS_SOURCE
+        valueParameterRenderer = KtValueParameterSymbolRenderer.AS_SOURCE
+        samConstructorRenderer = KtSamConstructorSymbolRenderer.NOT_RENDER
+
+        callableSignatureRenderer = KtCallableSignatureRender.FOR_SOURCE
+        accessorBodyRenderer = KtPropertyAccessorBodyRenderer.NO_BODY
+        parameterDefaultValueRenderer = KtParameterDefaultValueRenderer.NO_DEFAULT_VALUE
+        variableInitializerRenderer = KtVariableInitializerRenderer.NO_INITIALIZER
+
+        classOrObjectRenderer = KtNamedClassOrObjectSymbolRenderer.AS_SOURCE
+        typeAliasRenderer = KtTypeAliasSymbolRenderer.AS_SOURCE
+        anonymousObjectRenderer = KtAnonymousObjectSymbolRenderer.AS_SOURCE
+        singleTypeParameterRenderer = KtSingleTypeParameterSymbolRenderer.WITHOUT_BOUNDS
+        propertyAccessorsRenderer = KtPropertyAccessorsRenderer.NO_DEFAULT
+
+        receiverTyperRenderer = KtCallableReceiverTypeRenderer.WITH_IN_APPROXIMATION
+        returnTypeRenderer = KtCallableReturnTypeRenderer.WITH_OUT_APPROXIMATION
+
+        typeRenderer = KtTypeRendererForSource.WITH_QUALIFIED_NAMES
+        annotationRenderer = KtAnnotationRendererForSource.WITH_QUALIFIED_NAMES
+        declarationTypeApproximator = KtRendererTypeApproximator.TO_DENNOTABLE
+        returnTypeFilter = KtCallableReturnTypeFilter.NO_UNIT_FOR_FUNCTIONS
+    }
+
+    public val WITH_SHORT_NAMES: KtDeclarationRenderer = WITH_QUALIFIED_NAMES.with {
+        annotationRenderer = KtAnnotationRendererForSource.WITH_SHORT_NAMES
+        typeRenderer = KtTypeRendererForSource.WITH_SHORT_NAMES
+    }
+}
+

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/declarations/renderers/classifiers/KtAnonymousObjectSymbolRenderer.kt
@@ -1,0 +1,33 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.renderer.declarations.renderers.classifiers
+
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtDeclarationRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.renderAnnotationsAndModifiers
+import org.jetbrains.kotlin.analysis.api.symbols.KtAnonymousObjectSymbol
+import org.jetbrains.kotlin.analysis.utils.printer.PrettyPrinter
+import org.jetbrains.kotlin.lexer.KtTokens
+
+public interface KtAnonymousObjectSymbolRenderer {
+    context(KtAnalysisSession, KtDeclarationRenderer)
+    public fun renderSymbol(symbol: KtAnonymousObjectSymbol, printer: PrettyPrinter)
+
+    public object AS_SOURCE : KtAnonymousObjectSymbolRenderer {
+        context(KtAnalysisSession, KtDeclarationRenderer)
+        override fun renderSymbol(symbol: KtAnonymousObjectSymbol, printer: PrettyPrinter): Unit = printer {
+            " ".separated(
+                {
+                    " : ".separated(
+                        { renderAnnotationsAndModifiers(symbol, printer, KtTokens.OBJECT_KEYWORD) },
+                        { superTypeListRenderer.renderSuperTypes(symbol, printer) }
+                    )
+                },
+                { classifierBodyRenderer.renderBody(symbol, printer) },
+            )
+        }
+    }
+}

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/renderer/types/impl/KtTypeRendererForSource.kt
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.renderer.types.impl
+
+import org.jetbrains.kotlin.analysis.api.renderer.base.KtKeywordRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.base.annotations.KtAnnotationRendererForSource
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KtRendererTypeApproximator
+import org.jetbrains.kotlin.analysis.api.renderer.types.KtTypeRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.types.renders.*
+
+public object KtTypeRendererForSource {
+    public val WITH_QUALIFIED_NAMES: KtTypeRenderer = KtTypeRenderer {
+        capturedTypeRenderer = KtCapturedTypeRenderer.AS_RPOJECTION
+        definitelyNotNullTypeRenderer = KtDefinitelyNotNullTypeRenderer.AS_TYPE_INTERSECTION
+        dynamicTypeRenderer = KtDynamicTypeRenderer.AS_DYNAMIC_WORD
+        flexibleTypeRenderer = KtFlexibleTypeRenderer.AS_SHORT
+        functionalTypeRenderer = KtFunctionalTypeRenderer.AS_FUNCTIONAL_TYPE
+        integerLiteralTypeRenderer = KtIntegerLiteralTypeRenderer.AS_ILT_WITH_VALUE
+        intersectionTypeRenderer = KtIntersectionTypeRenderer.AS_INTERSECTION
+        typeErrorTypeRenderer = KtTypeErrorTypeRenderer.AS_CODE_IF_POSSIBLE
+        typeParameterTypeRenderer = KtTypeParameterTypeRenderer.AS_SOURCE
+        unresolvedClassErrorTypeRenderer = KtUnresolvedClassErrorTypeRenderer.UNRESOLVED_QUALIFIER
+        usualClassTypeRenderer = KtUsualClassTypeRenderer.AS_CLASS_TYPE_WITH_TYPE_ARGUMENTS
+        classIdRenderer = KtClassTypeQualifierRenderer.WITH_QUALIFIED_NAMES
+        typeNameRenderer = KtTypeNameRenderer.QUOTED
+        typeApproximator = KtRendererTypeApproximator.TO_DENNOTABLE
+        typeProjectionRenderer = KtTypeProjectionRenderer.WITH_VARIANCE
+        annotationsRender = KtAnnotationRendererForSource.WITH_QUALIFIED_NAMES
+        keywordRenderer = KtKeywordRenderer.AS_WORD
+    }
+
+    public val WITH_SHORT_NAMES: KtTypeRenderer = WITH_QUALIFIED_NAMES.with {
+        classIdRenderer = KtClassTypeQualifierRenderer.WITH_SHORT_NAMES_WITH_NESTED_CLASSIFIERS
+        annotationsRender = KtAnnotationRendererForSource.WITH_SHORT_NAMES
+    }
+}